047582815c35edaa31ae7f835632c445cbe2266c
vignesh bharanitharan <gagnantvignesh@gmail.com>
2020-09-29 13:48:33 +0530

    new port added from env

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 
-
+require('dotenv').config()
 const express = require('express')
 const app= express();
-app.listen(3000,()=>{
+const port = process.env.PORT
+app.listen(port,()=>{
   console.log("listening at 3000")
 })
 app.use(express.static('public'));
