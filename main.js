e7730b7a410a2fb175538cd5678e38a2066218e5
vignesh bharanitharan <gagnantvignesh@gmail.com>
2020-09-29 14:59:14 +0530

    this is the geolocation code3

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 require('dotenv').config()
 const express = require('express')
 const app= express();
-const port = process.env.PORT
-app.listen(port,()=>{
+//const port = process.env.PORT
+app.listen(3000,()=>{
   console.log("listening at 3000")
 })
 app.use(express.static('public'));
